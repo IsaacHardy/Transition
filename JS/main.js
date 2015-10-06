3600408192f66d9f04404034782b05312abd7d7c
Isaac Hardy <isaachardyDEV@gmail.com>
2015-10-06 18:10:13 -0400

    Changed javascript to simplier funciton - cleaned the css with more accurate attributes

--- JS/main.js
+++ JS/main.js
@@ -1,56 +1,85 @@
 (function (){
 
+// // Define variabels for each header
+// var one = $('#first-p');
+// var two = $('#second-p');
+// var three = $('#third-p');
+
+// // Start page refesh with closed with top open.
+// one.toggleClass('open');
+// two.toggleClass('hidden');
+// three.toggleClass('hidden');
+
+
 // On Click, adds class to ID first p
 $('.lone').on('click', 'h2', function(event) {
     // Define a variable equal to each paragraph class
-    var clicker = $('#first-p');
+    // var clicker = $('#first-p');
 
-    // If the paragraph has already changed class, remove class.
-    if (clicker.hasClass('slide-p')) {
-      clicker.removeClass('slide-p');
+    $('#first-p').toggleClass('hidden');
 
-    // If the class hasn't been changed, add new class and remove classes from other paragraphs.
-  } else {
+  //   // If the paragraph has already changed class, remove class.
+  //   if (clicker.hasClass('open')) {
+  //     clicker.toggleClass('hidden');
 
-    $('#first-p').addClass('slide-p');
-    $('#second-p').removeClass('slide-p');
-    $('#third-p').removeClass('slide-p');
-  }
+  //     // Remove class from other paras
+  //     $('#second-p').toggleClass('open');
+  //     $('#third-p').toggleClass('hidden');
+
+  //   // If the class hasn't been changed, add new class and remove classes from other paragraphs.
+  // } else {
+
+  //   $('#first-p').toggleClass('open');
+  //   $('#second-p').toggleClass('hidden');
+  //   $('#third-p').toggleClass('hidden');
+  // }
 });
 
 // On Click, adds class to ID second p
 $('.ltwo').on('click', 'h2', function(event) {
-    // Define a variable equal to each paragraph class
-    var clicker = $('#second-p');
+
+  $('#second-p').toggleClass('hidden');
+
+  //   // Define a variable equal to each paragraph class
+  //   var clicker = $('#second-p');
     
-    // If the paragraph has already changed class, remove class.
-    if (clicker.hasClass('slide-p')) {
-      clicker.removeClass('slide-p');
+  //   // If the paragraph has already changed class, remove class.
+  //   if (clicker.hasClass('open')) {
+  //     clicker.toggleClass('hidden');
 
-    // If the class hasn't been changed, add new class and remove classes from other paragraphs.
-  } else {
+  //     //
+  //     $('#first-p').toggleClass('open');
+  //     $('#third-p').toggleClass('hidden');
 
-    $('#second-p').addClass('slide-p');
-    $('#first-p').removeClass('slide-p');
-    $('#third-p').removeClass('slide-p');
-  }
+  //   // If the class hasn't been changed, add new class and remove classes from other paragraphs.
+  // } else {
+
+  //   $('#second-p').toggleClass('open');
+  //   $('#first-p').toggleClass('hidden');
+  //   $('#third-p').toggleClass('hidden');
+  // }
 });
 
 // On Click, adds class to ID third p
 $('.lthree').on('click', 'h2', function(event) {
-    // Define a variable equal to each paragraph class
-    var clicker = $('#third-p');
+
+  $('#third-p').toggleClass('hidden');
+  //   // Define a variable equal to each paragraph class
+  //   var clicker = $('#third-p');
     
-    // If the paragraph has already changed class, remove class.
-    if (clicker.hasClass('slide-p')) {
-      clicker.removeClass('slide-p');
+  //   // If the paragraph has already changed class, remove class.
+  //   if (clicker.hasClass('open')) {
+  //     clicker.toggleClass('hidden');
 
-    // If the class hasn't been changed, add new class and remove classes from other paragraphs.
-  } else {
+  //     $('#second-p').toggleClass('open');
+  //     $('#first-p').toggleClass('hidden');
 
-    $('#third-p').addClass('slide-p');
-    $('#second-p').removeClass('slide-p');
-    $('#first-p').removeClass('slide-p');
-  }
+  //   // If the class hasn't been changed, add new class and remove classes from other paragraphs.
+  // } else {
+
+  //   $('#third-p').toggleClass('open');
+  //   $('#second-p').toggleClass('hidden');
+  //   $('#first-p').toggleClass('hidden');
+  // }
 });
 }());
